33b603673b178ffb953649df002038934c7f2554
brandonlopez20 <brandonluares@gmail.com>
2022-10-11 17:36:34 -0500

    first commit

---
+++ src/main/java/com/mycompany/cursomvcjava/CursomvcJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.cursomvcjava;
+
+/**
+ *
+ * @author VP2021GT
+ */
+public class CursomvcJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
